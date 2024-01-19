fe0059ebd3b46510f486eda8c9a8ade7ea6aeb31
Tiago Gomes <tidgomes@gmail.com>
2024-01-19 13:56:12 -0300

    remove dummy tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,8 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
-const taskList = [
-  {
-    id: 1,
-    text: 'clean litter box',
-    day: 'Jan 23rd at 2:30pm',
-    reminder: true
-  },
-  { id: 2, text: 'go somewhere', day: 'Jan 30th at midnight', reminder: false },
-  { id: 3, text: 'do something', day: 'Feb 1st at noon', reminder: true }
-]
-
 function App() {
-  const [tasks, setTasks] = useState(taskList)
+  const [tasks, setTasks] = useState([])
 
   const noTasks = <p>No Tasks</p>
 
