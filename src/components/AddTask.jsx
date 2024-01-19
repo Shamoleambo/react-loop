f75cc12660930bc020c95110753ed9a9d202a410
Tiago Gomes <tidgomes@gmail.com>
2024-01-19 13:37:19 -0300

    make AddTask a controlled component

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,47 @@
+import { useState } from 'react'
+
 export default function AddTask() {
+  const [text, setText] = useState('')
+  const [day, setDay] = useState('')
+  const [reminder, setReminder] = useState(false)
+
+  const handleText = (event) => {
+    setText(event.target.value)
+  }
+
+  const handleDay = (event) => {
+    setDay(event.target.value)
+  }
+
+  const handleReminder = (event) => {
+    setReminder(event.currentTarget.checked)
+  }
+
   return (
     <form className='add-form'>
       <div className='form-control'>
         <label htmlFor='task'>Task</label>
-        <input type='text' id='task' placeholder='Add Task' />
+        <input
+          type='text'
+          id='task'
+          placeholder='Add Task'
+          onChange={handleText}
+          value={text}
+        />
       </div>
       <div className='form-control'>
         <label htmlFor='day&time'>Day & Time</label>
-        <input type='text' id='day&time' placeholder='Add Date' />
+        <input
+          type='text'
+          id='day&time'
+          placeholder='Add Date'
+          onChange={handleDay}
+          value={day}
+        />
       </div>
       <div className='form-control form-control-check'>
         <label htmlFor='reminder'>Set Reminder</label>
-        <input type='checkbox' id='reminder' />
+        <input type='checkbox' id='reminder' onChange={handleReminder} />
       </div>
 
       <input type='submit' value='Save Task' className='btn btn-block' />
