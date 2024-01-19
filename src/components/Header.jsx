702eb1807d9d26bd6ce4a81820e2cf36bd0d905f
Tiago Gomes <tidgomes@gmail.com>
2024-01-19 09:39:19 -0300

    create button component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import Button from "./Button"
 export default function Header({ title = 'Task Tracker' }) {
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' />
     </header>
   )
 }
