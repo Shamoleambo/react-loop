9842d33ab2adf2a0e10dc958cf7fd00c151a2fba
Tiago Gomes <tidgomes@gmail.com>
2024-01-19 09:30:10 -0300

    add styles to the index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
 export default function Header({ title = 'Task Tracker' }) {
   return (
     <header>
-      <h1 style={headingStyle}>{title}</h1>
+      <h1>{title}</h1>
     </header>
   )
 }
-
-const headingStyle = { color: 'red', backgroundColor: 'blue' }
